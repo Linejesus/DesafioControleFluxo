5da8361808908589e0e502b9496d172866919afd
Linejesus <161092862+Linejesus@users.noreply.github.com>
2024-06-25 21:59:39 -0300

    arrumando identação.java

--- controle-fluxo/src/Contador.java
+++ controle-fluxo/src/Contador.java
@@ -12,25 +12,26 @@ public class Contador {
 			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 
-            terminal.close();
+            		terminal.close();
 		
-		}catch (ParametrosInvalidosException exception) {
+		} catch (ParametrosInvalidosException exception) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+            		System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
 		}
 		
 	}
+	
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-        if (parametroUm > parametroDois) {
-            throw new ParametrosInvalidosException();
-        }
+        	if (parametroUm > parametroDois) {
+            		throw new ParametrosInvalidosException();
+        	}
 		else {
-            int contagem = parametroDois - parametroUm;
-            //realizar o for para imprimir os números com base na variável contagem
-            for (int i = 0; i < contagem; i++){
-                System.out.println("Imprimindo o número " + (i+1));
-            }
-        }
+            		int contagem = parametroDois - parametroUm;
+            		//realizar o for para imprimir os números com base na variável contagem
+            		for (int i = 0; i < contagem; i++){
+                		System.out.println("Imprimindo o número " + (i+1));
+            		}
+        	}
 	}
 }
